2e439cea8e92c2693be44f3370c45a7b227c63b2
Omthube23 <154900830+Omthube23@users.noreply.github.com>
2024-11-24 00:07:34 +0530

    R0

---
+++ src/main/java/com/Microsoft/Winss/WinssApplication.java
@@ -0,0 +1,13 @@
+package com.Microsoft.Winss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Microsoft/Winss/WinssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microsoft.Winss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
